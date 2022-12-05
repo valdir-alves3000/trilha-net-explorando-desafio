39e91d8b3c6bfa3bd3ca1f276348386ada60ddb5
Valdir Alves <valdiralves@gmail.com>
2022-12-04 21:06:40 -0300

    Challenge completed successfully

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,60 +1,66 @@
 namespace DesafioProjetoHospedagem.Models
 {
-    public class Reserva
+  public class Reserva
+  {
+    public List<Pessoa> Hospedes { get; set; }
+    public Suite Suite { get; set; }
+    public int DiasReservados { get; set; }
+
+    public Reserva() { }
+
+    public Reserva(int diasReservados)
     {
-        public List<Pessoa> Hospedes { get; set; }
-        public Suite Suite { get; set; }
-        public int DiasReservados { get; set; }
-
-        public Reserva() { }
-
-        public Reserva(int diasReservados)
-        {
-            DiasReservados = diasReservados;
-        }
-
-        public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
-        }
-
-        public void CadastrarSuite(Suite suite)
-        {
-            Suite = suite;
-        }
-
-        public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
-        }
-
-        public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
-
-            return valor;
-        }
+      DiasReservados = diasReservados;
     }
+
+    public void CadastrarHospedes(List<Pessoa> hospedes)
+    {
+      if (Suite.Capacidade >= hospedes.Count)
+      {
+        Hospedes = hospedes;
+      }
+      else
+      {
+
+        Console.WriteLine($"Falha no Cadastro. Esta Suite tem capacidade para {Suite.Capacidade} Hospedes");
+
+      }
+    }
+
+    public void CadastrarSuite(Suite suite)
+    {
+      Suite = suite;
+    }
+
+    public int ObterQuantidadeHospedes()
+    {
+      try
+      {
+        int qtdHospedes = Hospedes.Count;
+        return qtdHospedes;
+      }
+      catch (Exception)
+      {
+        DiasReservados = 0;
+        Console.WriteLine("Suite não Localizada");
+
+        return 0;
+
+      }
+    }
+
+    public decimal CalcularValorDiaria()
+    {
+
+      decimal valor = DiasReservados * Suite.ValorDiaria;
+
+      if (DiasReservados >= 10)
+      {
+        decimal descontoDiaria = valor * Convert.ToDecimal(0.10);
+
+        valor += -descontoDiaria;
+      }
+      return valor;
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -8,15 +8,25 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+Pessoa p5 = new Pessoa(nome: "Hóspede 5");
+Pessoa p6 = new Pessoa(nome: "Hóspede 6");
+Pessoa p7 = new Pessoa(nome: "Hóspede 7");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
+hospedes.Add(p5);
+hospedes.Add(p6);
+hospedes.Add(p7);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 8);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
